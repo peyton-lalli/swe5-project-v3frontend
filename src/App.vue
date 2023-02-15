e6730a6a9c50b528613077f6a68975627688ec54
ethanimooney <ethanimooney@gmail.com>
2023-02-15 15:48:26 -0600

    Remove excess comments

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <v-app>
     <v-main>
-      <!-- <StudentHomeDashboard></StudentHomeDashboard> -->
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  // import StudentHomeDashboard from "./views/StudentHomeDashboard.vue";
-
   export default {
     name: "App",
     components: {},

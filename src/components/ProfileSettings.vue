555c6bf4e6cc40de30324524082f589f86fc44c8
Peyton Lalli <peyton.lalli@eagles.oc.edu>
2023-02-25 22:32:52 -0600

    Best first rough draft for Profile Settings popup

--- src/components/ProfileSettings.vue
+++ src/components/ProfileSettings.vue
@@ -1,8 +1,9 @@
 <template>
   <v-card>
-    <v-card-title class="text-darkBlue">Profile Settings</v-card-title>
-    <v-card-subtitle>
-      <v-btn elevation="0">
+    <v-row>
+      <v-col><v-card-title class="text-darkBlue font-weight-bold">Profile Settings</v-card-title></v-col>
+      <v-col class="text-right">
+        <v-btn elevation="0" @click="closeDialog()">
         <v-icon>
           <font-awesome-icon
             icon="a-solid fa-circle-xmark"
@@ -10,64 +11,41 @@
           </font-awesome-icon>
         </v-icon>
       </v-btn>
-    </v-card-subtitle>
-      <v-card-text class="mx-auto">  
-        <!-- Profile Picture w/ Pencil Icon -->
-        <v-avatar class="bg-darkBlue" size="148">
-          <font-awesome-icon icon="fa-solid fa-user" class="text-white" />
-        </v-avatar> 
-        <v-btn elevation="0">
-          <v-icon size="large">
-            <font-awesome-icon
-              icon="fa-solid fa-pen"
-              class="text-darkBlue"
-            />
-          </v-icon>
-        </v-btn>
-      </v-card-text>
-      <v-card-text>
-        <!-- Name w/ Pencil Icon -->
-        <v-text-field 
-          bg-color="white"
-          placeholder="Piece Title" 
-          variant="solo" 
-          class="text-mediumBlue"
-          >
-        </v-text-field> 
-        <v-btn elevation="0">
-          <v-icon size="large">
-            <font-awesome-icon
-              icon="fa-solid fa-pen"
-              class="text-darkBlue"
-            />
-          </v-icon>
-        </v-btn>
-      </v-card-text>
+      </v-col>
+    </v-row>
       <v-card-text class="mx-auto">
-        <!-- This won't actually be a subtitle, just for display purposes for now-->
-        <v-card-subtitle >delta.roman@eagles.oc.edu</v-card-subtitle>
+        <v-avatar class="bg-darkBlue" size="148">
+          <v-img :src="userPictureLink"></v-img>
+        </v-avatar>
       </v-card-text>
-      <v-card-text>
+        <v-card-title class="mx-auto font-weight-bold">{{ userName.toUpperCase() }}</v-card-title>
+        <v-card-title class=" mx-auto text-lightBlue"> {{ userEmail }}</v-card-title>
+      <v-card-text v-if="userRole === 'student'">
         <v-select
           bg-color="lightBlue"
           class="text-blue"
-          placeholder="Major"
+          :placeholder="userTitleOrMajor"
           :items="['Vocal Music', 'Computer Science', 'Psychology']"
           variant="solo"
           text="darkBlue"
         ></v-select>
       </v-card-text>
-      <v-card-text>
+      <v-card-text v-if="userRole === 'faculty' || userRole === 'admin'">
+        <v-text-field>
+          <!-- sdvksndv,-->
+        </v-text-field>
+      </v-card-text>
+      <v-card-text v-if="userRole === 'student'">
         <v-select
           bg-color="lightBlue"
           class="text-blue"    
-          placeholder="Classification"
+          :placeholder="userClassification"
           :items="['Freshman', 'Sophomore', 'Junior', 'Senior', 'Graduate']"
           variant="solo"
           text="darkBlue"
         ></v-select>
       </v-card-text>
-      <v-card-text>  
+      <v-card-text v-if="userRole === 'student'">  
         <v-select
           bg-color="lightBlue"
           class="text-blue"    
@@ -77,21 +55,72 @@
           text="darkBlue"
         ></v-select>
       </v-card-text>
-      <v-card-actions class="mx-auto">
+      <v-card-actions class="mx-auto font-weight-bold">
           <v-btn color="darkBlue">Save</v-btn>
           <v-btn @click="closeDialog()" color="red">Cancel</v-btn>
       </v-card-actions>
       </v-card>
 </template>
 
+<style scoped>
+  .noDropShadow {
+    box-shadow: none !important;  
+  }
+</style>
+
 <script>
+  import InstructorsDataService from "../services/instructors.js";
+  import StudentInfoDataService from "../services/studentinfo.js";
+  import { useLoginStore } from "../stores/LoginStore.js";
+  import { mapStores } from "pinia";
+  
   export default {
     name: "EventComponent",
     components: {},
     data() {
-      return {};
+      return {
+        userRole: "",
+        userPictureLink: "",
+        userName: "Delta Roman",
+        userEmail: "delta.roman@oc.edu",
+        userTitleOrMajor: "",
+        userClassification: "Senior",
+        userSemesters: 8,
+      };
+    },
+    computed: {
+      ...mapStores(useLoginStore),
+    },
+    mounted() {
+      this.retrieveInfo();
     },
     methods: {
+      retrieveInfo() {
+        this.userRole = this.loginStore.loginUser.role;
+        this.userName = this.loginStore.getFullName;
+        this.userEmail = this.loginStore.loginUser.email;
+        this.userPictureLink = this.loginStore.loginUser.picture;
+        if (this.userRole === "student") {
+          StudentInfoDataService.getUserId(this.loginStore.loginUser.userId)
+            .then((response) => {
+              console.log(response.data);
+              this.userTitleOrMajor = response.data.StudentInfo[0].major;
+              // this.userSemesters = response.data.StudentInfo [0].semesters;
+              //this.userClassification = 
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        } else if (this.userRole === "faculty") {
+          InstructorsDataService.getSingle(this.loginStore.loginUser.userId)
+            .then((response) => {
+              this.userTitleOrMajor = response.data.Instructors[0].title;
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        }
+      },
       closeDialog() {
         this.$emit("closeCourseDialogEvent", false);
       },

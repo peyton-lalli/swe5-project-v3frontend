fe4f32c32887d6cfb031b040594c9729d2826c0e
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2023-03-29 19:12:36 -0500

    Roles Service
    This adds the services for the roles.
    
    - Creates a service file for the roles table.
    - Creates a service file for the userrole table.
    - Removes a service from the user service file that no longer is used.

---
+++ src/services/userrole.js
@@ -0,0 +1,16 @@
+import http from "./http-common";
+class UserRoleDataService {
+  create(data) {
+    return http.post("/performance-t3/userrole", data);
+  }
+  update(id, data) {
+    return http.put(`/performance-t3/userrole/${id}`, data);
+  }
+  getAll() {
+    return http.get("/performance-t3/userrole");
+  }
+  delete(id) {
+    return http.delete(`/performance-t3/userrole/${id}`);
+  }
+}
+export default new UserRoleDataService();
